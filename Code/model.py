5f0182f29877d8edd314efef8f24b9539e9dff91
SaulLu <lucilesaul.com@gmail.com>
2021-03-04 10:29:22 +0100

    fix log

--- Code/model.py
+++ Code/model.py
@@ -42,10 +42,10 @@ class Model(pl.LightningModule):
         x, y = batch
         y_hat = self(x)
         loss = self.criterion(y_hat, y)
-        self.log("val_loss", loss, on_step=True, on_epoch=True)
+        self.log("val_loss", loss, on_epoch=True)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
         loss = self.criterion(y_hat, y)
-        self.log("test_loss", loss, on_step=True, on_epoch=True)
+        self.log("test_loss", loss)
