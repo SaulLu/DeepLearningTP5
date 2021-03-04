b74b747b347774c5b55724092e498adcc0a8d820
SaulLu <lucilesaul.com@gmail.com>
2021-03-04 10:22:37 +0100

    log both on epoch and step

--- Code/model.py
+++ Code/model.py
@@ -35,16 +35,17 @@ class Model(pl.LightningModule):
         x, y = batch
         y_hat = self(x)
         loss = self.criterion(y_hat, y)
+        self.log("train_loss", loss, on_step=True, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
         loss = self.criterion(y_hat, y)
-        self.log("val_loss", loss)
+        self.log("val_loss", loss, on_step=True, on_epoch=True)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
         loss = self.criterion(y_hat, y)
-        self.log("test_loss", loss)
+        self.log("test_loss", loss, on_step=True, on_epoch=True)
