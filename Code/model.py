fa53ef90e2d66cf222d767ce933bb0a4e3de0fe2
SaulLu <lucilesaul.com@gmail.com>
2021-03-10 20:01:58 +0100

    clean for first training

--- Code/model.py
+++ Code/model.py
@@ -64,34 +64,23 @@ class Model(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         w_t1, w_t2, w_next = batch
-        # print(f"w_t2: {w_t2.shape}")
-        # print(f"w_next: {w_next.shape}")
-        # data.requires_grad = True  # this is essential!
         w_t2_pred = self(w_t1)
         w_next_pred = self.full_traj(11, w_t1, return_numpy=False)
-        # print(f"w_t2_pred: {w_t2_pred}")
-        # import dll
 
-        mse_w_t2 = self.criterion(w_t2, w_t2_pred)  # + self.lambda_jr * self.reg(data, output)
-        mse_w_next = self.criterion_2(w_next, w_next_pred)
-        loss = mse_w_t2 + mse_w_next
+        mse_w_t2 = self.criterion(w_t2, w_t2_pred)
+        mse_w_next = self.criterion_2(w_next, w_next_pred).sum() / w_next.shape[0]
+        loss = mse_w_t2 + 1 / w_next.shape[1] * mse_w_next
 
         self.log("train_loss", loss, on_step=True, on_epoch=True)
-        # self.log("train_mse_w_t2", mse_w_t2, on_epoch=True)
-        # self.log("train_mse_w_next", mse_w_next, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         w_t1, w_t2, w_next = batch
-        # print(f"w_t2: {w_t2.shape}")
-        # print(f"w_next: {w_next.shape}")
-        # data.requires_grad = True  # this is essential!
         w_t2_pred = self(w_t1)
         w_next_pred = self.full_traj(11, w_t1, return_numpy=False)
-        # print(f"w_t2_pred: {w_t2_pred}")
-        mse_w_t2 = self.criterion(w_t2, w_t2_pred)  # + self.lambda_jr * self.reg(data, output)
-        mse_w_next = self.criterion_2(w_next, w_next_pred)
-        loss = mse_w_t2 + mse_w_next
+        mse_w_t2 = self.criterion(w_t2, w_t2_pred)
+        mse_w_next = self.criterion_2(w_next, w_next_pred).sum() / w_next.shape[0]
+        loss = mse_w_t2 + 1 / w_next.shape[1] * mse_w_next
         self.log("val_loss", loss, on_epoch=True)
         self.log("val_mse_w_t2", mse_w_t2, on_epoch=True)
         self.log("val_mse_w_next", mse_w_next, on_epoch=True)
@@ -101,23 +90,6 @@ class Model(pl.LightningModule):
 
         pred_traj = outputs[-1]["w_next_pred"].cpu().numpy()
         true_traj = outputs[-1]["w_next"].cpu().numpy()
-        print(f"outputs[-1]['w_next_pred']: {outputs[-1]['w_next_pred'].shape}")
-        print(f"outputs[-1]['w_next']: {outputs[-1]['w_next'].shape}")
-        print(f"pred_traj: {pred_traj.shape}")
-        print(f"true_traj: {true_traj.shape}")
-        # pred_traj = None
-        # true_traj = None
-        # for output in outputs:
-        #     w_t2_pred = output["w_t2_pred"].cpu().numpy()
-        #     w_t2 = output["w_t2"].cpu().numpy()
-
-        #     if pred_traj is None:
-        #         pred_traj = w_t2_pred
-        #         true_traj = w_t2
-        #     true_traj = np.concatenate((true_traj, w_t2), axis=0)
-        #     pred_traj = np.concatenate((pred_traj, w_t2_pred), axis=0)
-        # print(f"true_traj: {true_traj.shape}")
-        # print(f"pred_traj: {pred_traj.shape}")
         ax, fig = plot3D_traj(pred_traj, true_traj)
         ax.scatter(true_traj[0][0], true_traj[0][1], true_traj[0][2], marker="o", label="true")
         ax.scatter(
@@ -135,15 +107,11 @@ class Model(pl.LightningModule):
 
     def test_step(self, batch, batch_idx):
         w_t1, w_t2, w_next = batch
-        # print(f"w_t2: {w_t2.shape}")
-        # data.requires_grad = True  # this is essential!
         w_t2_pred = self(w_t1)
         w_next_pred = self.full_traj(11, w_t1, return_numpy=False)
-        mse_w_t2 = self.criterion(w_t2, w_t2_pred)  # + self.lambda_jr * self.reg(data, output)
-        mse_w_next = self.criterion_2(w_next, w_next_pred)
-        loss = mse_w_t2 + mse_w_next
-
-        # self.log("test_loss", loss)
+        mse_w_t2 = self.criterion(w_t2, w_t2_pred)
+        mse_w_next = self.criterion_2(w_next, w_next_pred).sum() / w_next.shape[0]
+        loss = mse_w_t2 + 1 / w_next.shape[1] * mse_w_next
         self.log("test_mse", loss)
 
     def full_traj(self, nb_steps, init_pos, return_numpy=True):
@@ -151,7 +119,6 @@ class Model(pl.LightningModule):
             if len(init_pos.shape) == 1:
                 init_pos = init_pos[np.newaxis, :]
             init_pos = torch.tensor(init_pos, dtype=torch.float)
-        # print(f"initial_condition: {init_pos}")
 
         traj = [init_pos]
 
@@ -159,25 +126,17 @@ class Model(pl.LightningModule):
             if return_numpy:
                 for _ in tqdm(range(nb_steps - 1), position=0, leave=True):
                     new_coord = self(traj[-1]).detach()
-                    # print(f"new_coord: {new_coord.shape}")
-
                     traj.append(new_coord)
             else:
                 for _ in range(nb_steps - 1):
                     new_coord = self(traj[-1])
                     traj.append(new_coord)
-                # t.append(t[-1] + self.delta_t)
-        # traj = np.concatenate(traj, axis=0)
-        # traj = torch.cat(traj, axis=1)
         traj = torch.stack(traj, axis=1)
         if return_numpy:
             t = np.array([self.delta_t * step for step in range(nb_steps - 1)])
             traj = traj.squeeze()
             traj = traj.numpy()
-            # print(f"traj: {traj.shape}")
             return traj, t
-        # print(f"traj: {traj.shape}")
-        # t = np.array(t)
 
         return traj
 
