57a1170cf5813b3373ec81235d95d5b9ba23949d
unknown <mimen4070@gmail.com>
2021-03-10 15:21:45 +0100

    original version

--- Code/time_series.py
+++ Code/time_series.py
@@ -1,25 +1,25 @@
 import argparse
-
 import numpy as np
-
-# from scipy.interpolate import interp1d
+import torch
+import torch.nn as nn
+from scipy.interpolate import interp1d
 
 INITIAL_CONDITION = [-5.75, -1.6, 0.02]
 TRAJECTORY_DUR = 10000
 
 
 class Rossler_model:
-    def __init__(self, model_cls, checkpoint_path):
+    def __init__(self, delta_t, model_cls, checkpoint_path):
+        self.delta_t = delta_t 
+        self.nb_steps = int(TRAJECTORY_DUR // self.delta_t)  # int(10000 // self.delta_t)
         self.rosler_nn = model_cls.load_from_checkpoint(checkpoint_path=checkpoint_path)
-        self.nb_steps = int(TRAJECTORY_DUR // self.rosler_nn.delta_t)  # int(10000 // self.delta_t)
         self.rosler_nn.normalize = True
         self.initial_condition = np.array(INITIAL_CONDITION)
 
     def full_traj(self, initial_condition=np.array(INITIAL_CONDITION), y_only=True):
-        traj, t = self.rosler_nn.full_traj(self.nb_steps, initial_condition)
+        traj, t = self.rosler_nn.full_traj(TRAJECTORY_DUR, initial_condition)
         if y_only:
             traj = traj[:, 1]
-        # TODO: warning interpolate trajectories is not done yet
         return traj
 
     def save_traj(self, y):
@@ -30,6 +30,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--init", nargs="+", type=float, default=[-5.75, -1.6, 0.02])
     value = parser.parse_args()
-    ROSSLER = Rossler_model()
+    ROSSLER = Rossler_model(delta_t)
     y = ROSSLER.full_traj()
     ROSSLER.save_traj(y)
