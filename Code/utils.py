ade4719cde0089db66efd2de434c5d0647c0ee94
unknown <mimen4070@gmail.com>
2021-03-10 16:14:55 +0100

    fixed by Lucile

--- Code/utils.py
+++ Code/utils.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import torch
+import numpy as np
 from numpy.linalg import norm, qr, solve
-from scipy.fftpack import fft, fftshift
 from scipy.linalg import expm
-
+from scipy.fftpack import fft, fftshift
 import wandb
 
 
@@ -16,7 +15,6 @@ def plot3D_traj(traj_pred, traj_true):
     ax.legend()
     return ax, fig
 
-
 def compute_traj(trained_model, rossler_map_true, initial_condition, nb_step):
     if isinstance(initial_condition, tuple) or isinstance(initial_condition, list):
         initial_condition = np.array(initial_condition)
@@ -24,18 +22,19 @@ def compute_traj(trained_model, rossler_map_true, initial_condition, nb_step):
     traj_true, time_list = rossler_map_true.full_traj(init_pos=initial_condition, nb_steps=nb_step)
     return traj_pred, traj_true, time_list
 
-
 class Statistics:
-    def __init__(self, wandb_logger, ts_n=10000, fft_n=1000):
+    def __init__(self, wandb_logger, delta_t=1e-2, ts_n=10000, fft_n=1000):
         self.wandb_logger = wandb_logger
+        self.delta_t = delta_t
         self.fft_n = fft_n
         self.ts_n = ts_n
         self.axis_names = ["x", "y", "z"]
+        self.n_bins = 20
 
     def add_traj(self, traj_true, traj_pred, time_list, prefix=None):
         self.traj_true = traj_true
         self.traj_pred = traj_pred
-        self.time_list = time_list
+        self.time_list = np.asarray(time_list)
         self.prefix = prefix
 
     def log_plot(self, ax, fig, title):
@@ -53,8 +52,8 @@ class Statistics:
         fig.set_figwidth(15)
         fig.set_figheight(5)
         for idx in range(3):
-            ax[idx].hist(self.traj_true[:, idx], label="true", alpha=0.8)
-            ax[idx].hist(self.traj_pred[:, idx], label="pred", alpha=0.8)
+            ax[idx].hist(self.traj_true[:, idx], label="true", alpha=0.8, bins=self.n_bins)
+            ax[idx].hist(self.traj_pred[:, idx], label="pred", alpha=0.8, bins=self.n_bins)
             ax[idx].legend()
             ax[idx].set_ylabel(self.axis_names[idx])
         self.log_plot(ax, fig, "PDF")
@@ -76,11 +75,16 @@ class Statistics:
         fig, ax = plt.subplots(3, 1)
         fig.set_figwidth(15)
         fig.set_figheight(15)
+        x_corr = np.linspace(-T // 2, T // 2, T)
         for idx in range(3):
-            corr_true = np.correlate(self.traj_true[:T, idx], self.traj_true[:T, idx], "same")
-            corr_pred = np.correlate(self.traj_pred[:T, idx], self.traj_pred[:T, idx], "same")
-            ax[idx].plot(self.time_list[:T], corr_true, "-.", label="true")
-            ax[idx].plot(self.time_list[:T], corr_pred, "-.", label="pred")
+            corr_true = np.correlate(
+                self.traj_true[:T, idx], self.traj_true[:T, idx], "same"
+            )
+            corr_pred = np.correlate(
+                self.traj_pred[:T, idx], self.traj_pred[:T, idx], "same"
+            )
+            ax[idx].plot(x_corr, corr_true, "-.", label="true")
+            ax[idx].plot(x_corr, corr_pred, "-.", label="pred")
             ax[idx].legend()
             ax[idx].set_ylabel(self.axis_names[idx])
         self.log_plot(ax, fig, "Time correlations")
@@ -90,8 +94,10 @@ class Statistics:
         for idx in range(3):
             spec_true = self.compute_spec(self.traj_true[:, idx])
             spec_pred = self.compute_spec(self.traj_pred[:, idx])
-            ax[idx].plot(spec_true, "-.", label="true")
-            ax[idx].plot(spec_pred, "-.", label="pred")
+            fmax = 1/(len(spec_true) * self.delta_t)
+            x_fft = np.linspace(-0.5, 0.5, len(spec_true))
+            ax[idx].plot(x_fft, spec_true, "-.", label="true")
+            ax[idx].plot(x_fft, spec_pred, "-.", label="pred")
             ax[idx].legend()
             ax[idx].set_ylabel(self.axis_names[idx])
         self.log_plot(ax, fig, "FFT")
@@ -139,11 +145,11 @@ class Dynamics:
         self.delta_t = trained_model.delta_t
         self.max_it = max_it
         self.max_comp = max_comp
-
+    
     def add_traj(self, traj_true, traj_pred):
         self.traj_true = traj_true
         self.traj_pred = traj_pred
-
+        
     def lyapunov_exponent(self, traj, jacobian, mode="discrete"):
         n = traj.shape[1]
         w = np.eye(n)
@@ -154,37 +160,33 @@ class Dynamics:
                 w_next = np.dot(jacob, w)
             elif mode == "continuous":
                 w_next = np.dot(expm(jacob * self.delta_t), w)
-        w_next, r_next = qr(w_next)
-        d = np.diag(np.sign(r_next.diagonal()))
-        w_next = np.dot(w_next, d)
-        r_next = np.dot(d, r_next.diagonal())
-        rs.append(r_next)
-        w = w_next
+            w_next, r_next = qr(w_next)
+            d = np.diag(np.sign(r_next.diagonal()))
+            w_next = np.dot(w_next, d)
+            r_next = np.dot(d, r_next.diagonal())
+            rs.append(r_next)
+            w = w_next
         return np.mean(np.log(rs), axis=0) / self.delta_t
-
+    
     def newton(self, f, jacob, x):
         tol = x
-        for compt in range(self.compt_max):
+        for compt in range(self.max_comp):         
             x = x - solve(jacob(x), f(x))
             tol = norm(tol - x)
-            if tol <= 1e-5:
+            if (tol <= 1e-5):
                 return x
         return x
 
     def compute_lyaponov(self):
-        lyap_pred = self.lyapunov_exponent(
-            self.traj_pred, self.trained_model.jacobian, mode="discrete"
-        )
-        lyap_true = self.lyapunov_exponent(
-            self.traj_true, self.true_model.jacobian, mode="continuous"
-        )
+        lyap_pred = self.lyapunov_exponent(self.traj_pred, self.trained_model.jacobian, mode="discrete")
+        lyap_true = self.lyapunov_exponent(self.traj_true, self.true_model.jacobian, mode="continuous")
         lyap_error = np.abs(lyap_pred - lyap_true)
         print(f"lyap_true: {lyap_true}," f"lyap_pred: {lyap_pred}," f"lyap_error : {lyap_error}")
         if self.wandb_logger is not None:
             self.wandb_logger.experiment.log(
-                {"lyap_pred": lyap_pred, "lyap_true": lyap_true, "lyap_error": lyap_error}
+                {f"lyap_pred": lyap_pred, "lyap_true": lyap_true, "lyap_error": lyap_error}
             )
-
+    
     def compute_equilibrum(self):
         init_pos = np.array([-5.75, -1.6, 0.02])
 
@@ -194,12 +196,12 @@ class Dynamics:
                 (self.trained_model.jacobian(torch.tensor(w, dtype=torch.float)) - torch.eye(3))
                 / self.delta_t
             )
-            .cpu()
+            .detach()
             .numpy()
         )
 
         f_system = (
-            lambda w: (self.trained_model(torch.tensor(w, dtype=torch.float)).cpu().numpy() - w)
+            lambda w: (self.trained_model(torch.tensor(w, dtype=torch.float)).detach().numpy() - w)
             / self.delta_t
         )
 
@@ -221,7 +223,7 @@ class Dynamics:
                     "fix_point_error": fix_point_error,
                 }
             )
-
+    
     def plot_all(self):
         self.compute_lyaponov()
         self.compute_equilibrum()
